24dc939ec283b43da655b25c7db1b0cbf049eecd
Andrew Carroll <mrandrewcarroll@gmail.com>
2016-04-20 18:00:17 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+Fjorum::Application.config.secret_token = 'b0ab148c0ba8dc62dfec4ff546847ea3c0469a7a96ba20ee80ce050216bda23bfc2b42252669449e5ff82997b7cc9bccdc4c986dc431d5c77830383ec443d577'
